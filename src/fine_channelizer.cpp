384aa21d62395abd00f2195989686d46dc521d2b
jebbailey <baileyji@ucsb.edu>
2022-09-11 23:35:22 -0500

    add variant with combined output and external BRAM for control. DDS block is registered.

--- src/fine_channelizer.cpp
+++ src/fine_channelizer.cpp
@@ -86,12 +86,84 @@ void resonator_ddc_control(hls::stream<axisdata_t> &res_in, tonegroup_t tones[N_
 }
 
 
-void dds_ddc_center(hls::stream<axisdata_t> &res_in, hls::stream<accgroup_t> &accumulator, hls::stream<loopcenter_group_t> &center,
-		hls::stream<axisdata_t> &res_out) {
+void resonator_ddc_control_mem(hls::stream<axisdata_t> &res_in, tones_and_centers_t tones_and_centers[N_RES_GROUPS],
+		hls::stream<axisdata_acc_center_combo_t> &out){
+//		hls::stream<axisdata_t> &res_out, hls::stream<accgroup_t> &acc_out, hls::stream<loopcenter_group_t> &center_out){
+bool clear_accumulator=false;
+#pragma HLS INTERFACE mode=bram port=tones_and_centers storage_type=ram_s2p latency=3
 #pragma HLS INTERFACE mode=axis register_mode=off port=res_in
-#pragma HLS INTERFACE mode=axis register_mode=off port=accumulator
-#pragma HLS INTERFACE mode=axis register_mode=off port=center
-#pragma HLS INTERFACE mode=axis port=res_out register_mode=off
+#pragma HLS INTERFACE mode=axis port=out register_mode=both
+//#pragma HLS INTERFACE mode=axis port=acc_out
+//#pragma HLS INTERFACE mode=axis port=center_out
+//#pragma HLS INTERFACE mode=axis port=res_out
+#pragma HLS INTERFACE ap_ctrl_none port=return
+//#pragma HLS INTERFACE s_axilite port=tones
+//#pragma HLS INTERFACE s_axilite port=centers
+//#pragma HLS INTERFACE s_axilite port=clear_accumulator
+#pragma HLS PIPELINE II=1
+
+	static accgroup_t accumulator[N_RES_GROUPS], accg;
+	static bool clear=false;
+
+	axisdata_t data_in;
+	group_t group;
+	tonegroup_t tonesgroup;
+	loopcenter_group_t centers;
+	tones_and_centers_t tone_and_center;
+	accgroup_t _accg, _phaseacc;
+
+	res_in.read(data_in);
+
+	group=data_in.user;
+
+	tone_and_center = tones_and_centers[group];
+
+	tonesgroup.range() = tone_and_center.range(255,0);
+	centers.range() = tone_and_center.range(511,256);
+
+	accumulator[group_t(group-1)]=accg;
+	_accg=accumulator[group];
+
+	incp: for (int i=0; i<N_RES_PCLK; i++) {
+		phase_t phase0, tmp;
+		toneinc_t inc;
+
+		inc.range()=tonesgroup.range(N_TONEBITS*(i+1)-1, N_TONEBITS*i);
+		phase0.range()=tonesgroup.range(N_P0BITS*(i+1)-1+N_TONEBITS*N_RES_PCLK,N_P0BITS*i+N_TONEBITS*N_RES_PCLK);
+
+		tmp.range()=_accg.range(NBITS*(i+1)-1, NBITS*i);
+
+		_phaseacc.range(NBITS*(i+1)-1, NBITS*i) = phase_t(tmp+phase0).range();
+		_accg.range(NBITS*(i+1)-1, NBITS*i) = phase_t(tmp+inc).range();
+	}
+
+//	center_out.write(centers);
+//	res_out.write(data_in);
+//	acc_out.write(_phaseacc);
+	axisdata_acc_center_combo_t _output;
+	_output.data.range(SAMPLE_GROUP_BITS-1,0)=data_in.data.range();
+	_output.data.range(ACC_GROUP_BITS+SAMPLE_GROUP_BITS-1, SAMPLE_GROUP_BITS)=_phaseacc.range();
+	_output.data.range(ACC_GROUP_BITS+SAMPLE_GROUP_BITS+SAMPLE_GROUP_BITS-1, ACC_GROUP_BITS+SAMPLE_GROUP_BITS)=centers.range();
+	_output.user=data_in.user;
+	_output.last=data_in.last;
+	out.write(_output);
+
+	accg = clear ? accgroup_t(0): _accg;
+	if (clear_accumulator)
+		clear=true;
+	else if (data_in.last)
+		clear=false;
+
+}
+
+void dds_ddc_center(hls::stream<axisdata_acc_center_combo_t> &ddc_control_out,
+		//hls::stream<axisdata_t> &res_in, hls::stream<accgroup_t> &accumulator, hls::stream<loopcenter_group_t> &center,
+		hls::stream<axisdata_t> &res_out) {
+#pragma HLS INTERFACE mode=axis register_mode=both port=ddc_control_out
+//#pragma HLS INTERFACE mode=axis register_mode=off port=res_in
+//#pragma HLS INTERFACE mode=axis register_mode=off port=accumulator
+//#pragma HLS INTERFACE mode=axis register_mode=off port=center
+#pragma HLS INTERFACE mode=axis port=res_out //register_mode=off
 #pragma HLS INTERFACE mode=ap_ctrl_none port=return
 #pragma HLS PIPELINE II=1
 	iqgroup_uint_t _out;
@@ -99,9 +171,16 @@ void dds_ddc_center(hls::stream<axisdata_t> &res_in, hls::stream<accgroup_t> &ac
 	loopcenter_group_t centergroupv;
 	axisdata_t data_out, data_in;
 
-	res_in.read(data_in);
-	accumulator.read(accg);
-	center.read(centergroupv);
+	axisdata_acc_center_combo_t _input;
+	ddc_control_out.read(_input);
+	data_in.data.range()=_input.data.range(SAMPLE_GROUP_BITS-1,0);
+	data_in.user=_input.user;
+	data_in.last=_input.last;
+	accg.range()=_input.data.range(ACC_GROUP_BITS+SAMPLE_GROUP_BITS-1, SAMPLE_GROUP_BITS);
+	centergroupv.range()=_input.data.range(ACC_GROUP_BITS+SAMPLE_GROUP_BITS+SAMPLE_GROUP_BITS-1, ACC_GROUP_BITS+SAMPLE_GROUP_BITS);
+//	res_in.read(data_in);
+//	accumulator.read(accg);
+//	center.read(centergroupv);
 
 	ddc: for (int i=0; i<N_RES_PCLK; i++) {
 		sampleout_complex_t iqout;
--- src/fine_channelizer.hpp
+++ src/fine_channelizer.hpp
@@ -13,6 +13,7 @@
 
 #define N_CYCLES 10
 
+#define SAMPLE_BITS 16
 #define N_RES_GROUPS 256
 #define N_GROUP_BITS 8
 
@@ -20,19 +21,23 @@
 #define N_TONEBITS 11
 #define N_P0BITS 21
 
+#define SAMPLE_GROUP_BITS (SAMPLE_BITS*2*N_RES_PCLK)
+#define ACC_GROUP_BITS (N_RES_PCLK*NBITS)
+
 typedef ap_uint<N_GROUP_BITS> group_t;
-typedef ap_fixed<16, 1, AP_RND_CONV, AP_SAT_SYM> sample_t;
-typedef ap_fixed<16, 1, AP_RND_CONV, AP_SAT_SYM> sampleout_t;
+typedef ap_fixed<SAMPLE_BITS, 1, AP_RND_CONV, AP_SAT_SYM> sample_t;
+typedef ap_fixed<SAMPLE_BITS, 1, AP_RND_CONV, AP_SAT_SYM> sampleout_t;
 typedef ap_fixed<N_TONEBITS, 1, AP_RND_CONV, AP_WRAP> toneinc_t;
 typedef ap_fixed<N_P0BITS, 1, AP_RND_CONV, AP_WRAP> phase_t; //-1-1 wrap
 typedef std::complex<sample_t> loopcenter_t;
-typedef ap_uint<256> loopcenter_group_t;
-typedef ap_uint<256> iqgroup_uint_t;
+typedef ap_uint<SAMPLE_GROUP_BITS> loopcenter_group_t;
+typedef ap_uint<SAMPLE_GROUP_BITS> iqgroup_uint_t;
 typedef sample_t center_t;
 typedef std::complex<sample_t> sample_complex_t;
 typedef std::complex<sampleout_t> sampleout_complex_t;
 typedef lut_word_t dds_t;
 typedef std::complex<dds_t> dds_complex_t;
+typedef ap_uint<SAMPLE_BITS*2*N_RES_PCLK*2> tones_and_centers_t;
 
 typedef struct {
 	sampleout_complex_t data[N_RES_PCLK];
@@ -49,7 +54,10 @@ typedef ap_axiu<256,8,0,0> axisdata_t;
 
 typedef ap_uint<(N_P0BITS+N_TONEBITS)*N_RES_PCLK> tonegroup_t;
 
-typedef ap_uint<NBITS*N_RES_PCLK> accgroup_t;
+typedef ap_uint<ACC_GROUP_BITS> accgroup_t;
+
+typedef ap_axiu<SAMPLE_GROUP_BITS+ACC_GROUP_BITS+SAMPLE_GROUP_BITS,8,0,0> axisdata_acc_center_combo_t;  //256*3
+
 
 void resonator_ddc_control(hls::stream<axisdata_t> &res_in, tonegroup_t tones[N_RES_GROUPS], loopcenter_group_t centers[N_RES_GROUPS],
 		hls::stream<axisdata_t> &res_out, hls::stream<accgroup_t> &acc_out, hls::stream<loopcenter_group_t> &center_out, bool clear);
